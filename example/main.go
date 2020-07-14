4108e72e50df06069d236ff66e8a4108c1ff8938
saiumesh535 <d.sai535@gmail.com>
2020-07-14 19:26:37 +0530

    added example readme

--- example/main.go
+++ example/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"ferry"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/ferry-go/ferry"
 
 	"github.com/go-playground/validator/v10"
 )
@@ -20,9 +21,21 @@ func main() {
 	config := ferry.Config{
 		Validator: validate,
 	}
+
 	app := ferry.InitServer(&config)
+
 	app.Get("/", func(ctx *ferry.Ctx) error {
 		return ctx.Send(http.StatusOK, "Hello, World")
+	})
+
+	// redirect to new url
+	app.Get("/redirect", func(ctx *ferry.Ctx) error {
+		return ctx.Redirect(http.StatusTemporaryRedirect, "http://localhost:3000/static")
+	})
+
+	app.Get("/name/:name", func(ctx *ferry.Ctx) error {
+		name := ctx.GetParam("name")
+		return ctx.Send(http.StatusOK, fmt.Sprintf("hello, %s", name))
 	})
 
 	app.Post("/login", func(ctx *ferry.Ctx) error {
@@ -34,6 +47,28 @@ func main() {
 		return ctx.Json(http.StatusOK, map[string]string{
 			"message": fmt.Sprintf("Welcome %s", login.Username),
 		})
+	})
+
+	// Grouping your route
+	auth := app.Group("/auth")
+	auth.Get("/login", func(ctx *ferry.Ctx) error {
+		return ctx.Send(http.StatusOK, "Hello, World")
+	})
+
+	// path and dir name
+	app.ServerDir("/static", "static")
+
+	// single file
+	app.ServeFile("/js", "static/login.js")
+
+	// Downloading file
+	app.Get("/download", func(ctx *ferry.Ctx) error {
+		return ctx.SendAttachment("static/login.js", "login.js")
+	})
+
+	// uploading file
+	app.Post("/upload", func(ctx *ferry.Ctx) error {
+		return ctx.UploadFile("static/login.js", "login.js")
 	})
 
 	log.Fatal(app.Listen("localhost:3000"))
