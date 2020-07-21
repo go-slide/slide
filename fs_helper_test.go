344c5ccd8c30a5bbb61654306a317425e2bbc8ba
saiumesh535 <d.sai535@gmail.com>
2020-07-21 12:37:32 +0530

    fixed test

--- fs_helper_test.go
+++ fs_helper_test.go
@@ -1,13 +1,15 @@
 package ferry
 
 import (
+	"github.com/go-playground/assert/v2"
 	"testing"
 )
 
 func TestAllPaths(t *testing.T) {
 	var paths []string
-	err := getAllPaths("./", &paths)
+	err := getAllPaths("example", &paths)
 	if err != nil {
 		t.Errorf("error while getting paths %s", err.Error())
 	}
+	assert.Equal(t, len(paths), 1)
 }
